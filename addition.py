05e42188ae96a4c59e995e80ae16b349739f5ca5
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-29 21:40:34 +0000

    addition

---
+++ addition.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by Donggeun Lim
+# Created on June 2020
+# This program is addition
+# with user input
+
+
+def main():
+    # this function ia addition
+
+    # input
+    first_number = int(input("Enter first number : "))
+    second_number = int(input("Enter second number : "))
+
+    # process
+    value = first_number+second_number
+
+    # output
+    print("")
+    print("value is {}".format(value))
+
+
+if __name__ == "__main__":
+    main()
